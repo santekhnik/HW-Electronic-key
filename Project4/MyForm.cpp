8705d74c9564379269a9b68a345abd4e0c84e17a
AlexandrGashevsky <71451324+AlexandrGashevsky@users.noreply.github.com>
2023-01-25 00:28:10 +0200

    ui first version

---
+++ Project4/MyForm.cpp
@@ -0,0 +1,11 @@
+#include "MyForm.h"
+
+using namespace Project4;
+
+[STAThreadAttribute]
+int main(array<System::String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm());
+	return 0;
+}
