5bc52d3024eba1d1eaa7199f7b565759dd3c88d0
Yura <antimoob@gmail.com>
2023-02-06 21:13:49 +0200

    flash

---
+++ Flash_storage/MDK-ARM/RTE/_TEST_Flash/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TEST_Flash' 
+ * Target:  'TEST_Flash' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
